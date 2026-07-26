0d1bc312c0e776af28f999592b3dfbec4df56ae6
Gaboquerizo <gabrielbaquerizo2000@gmail.com>
2026-07-26 13:38:43 -0500

    ci: configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: '/portfolio/',
-    build: {
-        outDir: 'docs',
-        emptyOutDir: true,
-    },
+    base: '/portfolio/'
 });
